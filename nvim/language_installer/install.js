4eac50bc0225956c54248f1c53dd6493bde478bb
AananthV <aananthv@outlook.com>
2021-06-03 15:35:09 +0530

    fix: oopsies

--- nvim/language_installer/install.js
+++ nvim/language_installer/install.js
@@ -6,7 +6,7 @@ const { execSync } = require('child_process');
 
 const languages = process.argv.slice(2);
 
-const coc_config = {}
+let coc_config = {}
 
 // Install language
 for (let language of languages) {
@@ -15,7 +15,7 @@ for (let language of languages) {
             execSync(installer);
         }
         
-        const language_server_config = path.join(__dirname, `installer/${language}/config.json`);
+        const language_server_config = path.join(__dirname, `installer/${language}/coc.json`);
         if (fs.existsSync(language_server_config)){
                 const language_config = require(language_server_config);
                 coc_config = {...coc_config, ...language_config};
--- nvim/language_installer/installer/java/install.sh
+++ nvim/language_installer/installer/java/install.sh
@@ -1,1 +1,9 @@
+# Install jdk
+apk add openjdk11
+
+# Install coc-java
 nvim --headless +"CocInstall coc-java -sync" +qall
+
+# Install jdt.ls
+mkdir -p /root/.config/coc/extensions/coc-java-data/server
+curl http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz | tar -xz -C /root/.config/coc/extensions/coc-java-data/server
